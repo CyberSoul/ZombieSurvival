486593955b6171e64659fa6d7941977e3f406b89
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-02-02 23:44:22 +0200

    +added minimap

---
+++ ZombieSurvival/Assets/Scripts/MiniMap/MiniMap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public Transform m_player;
+    //private float m_initialY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = m_player.transform.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        //roatteion of camera same as player rotation but it is not needed for me.
+        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, m_player.eulerAngles.y, transform.rotation.eulerAngles.z);
+    }
+}
