c2c0e19c395ef9c026e5e89ce7f324fb9d1af45b
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-02-02 17:54:49 +0200

    +added pause

---
+++ ZombieSurvival/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour
+{
+    public GameObject m_paussedMenu;
+    bool m_isPaused = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PausePressed()
+    {
+        m_isPaused = !m_isPaused;
+        m_paussedMenu.SetActive(m_isPaused);
+        Time.timeScale = m_isPaused ? 0 : 1;
+    }
+}
