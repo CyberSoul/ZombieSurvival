f721be4617f3954ea45d1e6cef9284ff9163387e
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-02-21 00:12:28 +0200

    -reworked exit button to back in game mode. Added exit button into main menu

--- ZombieSurvival/Assets/Scripts/Menu/MainMenuController.cs
+++ ZombieSurvival/Assets/Scripts/Menu/MainMenuController.cs
@@ -18,15 +18,9 @@ public class MainMenuController : MonoBehaviour
         {
             m_levelList = new List<FileInfo>();
             List<string> levelList = new List<string>();
-            /*levelList.Add("Level 1");
-            levelList.Add("Level 2");*/
             string lvlPath = $"{Application.dataPath}/{m_path}";
             DirectoryInfo lvlDirectory = new DirectoryInfo(lvlPath);
-            //m_levelList = lvlDirectory.GetFiles("*.psd|*.png");
-
             m_levelList.AddRange(lvlDirectory.GetFiles("*.png"));
-            //m_levelList.AddRange(lvlDirectory.GetFiles("*.psd"));// lvlDirectory.GetFiles("*.psd");
-
 
             foreach (var file in m_levelList)
             {
@@ -38,24 +32,11 @@ public class MainMenuController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    /*void Update()
-    {
-        
-    }*/
-
     public void LoadLevel()
     {
         FileInfo file = m_levelList[m_listSelector.value];
-        var test = file.FullName;
-        //var test2 = Resources.Load(file.FullName);
-
-        /*string testPath = "Levels/level_test_4";
-        Texture2D lvlSprite = Resources.Load<Texture2D>(testPath);*/
         Texture2D lvlSprite = LoadImage(file.FullName);
-
         StaticDataHandler.LoadedLevel = lvlSprite;
-
         SceneManager.LoadScene((int)Scenes.Game, LoadSceneMode.Single);
     }
 
@@ -66,7 +47,7 @@ public class MainMenuController : MonoBehaviour
             byte[] fileData;
             fileData = File.ReadAllBytes(filePath);
             Texture2D result = new Texture2D(2, 2);
-            result.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+            result.LoadImage(fileData);
 
             return result;
         }
